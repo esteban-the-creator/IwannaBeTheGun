e574c8c290fa08634866c2efe1a2014cc0f5867c
Esteban <estebangs98@gmail.com>
2019-09-06 19:47:59 -0500

    begin
    instructuions to begin

---
+++ New Unity Project/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+
+
+
+
+/*
+ el jugador debe subir automaticamente un valor aleatorio de escalas
+ El valor aleatorio de escalas subidas las determina la cantidad de escalas que se instancian automaticamente 
+ se generan son 3 bloques de escalas para empezar, y siempre qeu se sube un boque se crea otro
+ por cada bloque de escalas que se sube se deben generar nuevos enemigos prefab (al final de cada bloque debe haber como una referencia a que ahi debe aparecer el enemigo)
+ se dispara en un rango de accion que se va moviendo continuamente en arco
+ al disparar el jugador se pasa al turno de disparar del enemigo (deberia ser al azar tambien)
+ 
+     
+     */
+}
